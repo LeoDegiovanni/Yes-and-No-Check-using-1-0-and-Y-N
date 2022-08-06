0dd4a79eba74ff64cf38de8df00615390b9d0511
LeoDegiovanni <109441881+LeoDegiovanni@users.noreply.github.com>
2022-08-06 10:25:26 -0700

    Update Checking_For_Yes_Or_No.py

--- Checking_For_Yes_Or_No.py
+++ Checking_For_Yes_Or_No.py
@@ -7,12 +7,7 @@ while 1 == 1:
     x = int(x)   
         
     while x < 0 or x > 1:
-            x = int(input("Type 0 to say no, and 1 to say yes: "))     
-    
-    x = str(x)
-    
-    while len(x) == 0 or len(x) > 1:
-                x = str(input("Type 0 to say no, and 1 to say yes: "))
+            x = int(input("Type 0 to say no, and 1 to say yes: "))
     break
 
 x = int(x)
