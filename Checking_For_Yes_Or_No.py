279547d99d2474be5a991a66581a761cdcbbf2fc
LeoDegiovanni <109441881+LeoDegiovanni@users.noreply.github.com>
2022-08-05 21:01:57 -0700

    Add files via upload

---
+++ Checking_For_Yes_Or_No.py
@@ -0,0 +1,23 @@
+x = str(input("Type 0 to say no, and 1 to say yes: "))
+
+while 1 == 1:  
+    while len(x) == 0 or len(x) > 1:
+        x = str(input("Type 0 to say no, and 1 to say yes: "))
+    
+    x = int(x)   
+        
+    while x < 0 or x > 1:
+            x = int(input("Type 0 to say no, and 1 to say yes: "))     
+    
+    x = str(x)
+    
+    while len(x) == 0 or len(x) > 1:
+                x = str(input("Type 0 to say no, and 1 to say yes: "))
+    break
+
+x = int(x)
+
+if x == 1:
+    print("You said yes!")
+elif x == 0:
+    print("You said no!")
